9b57a97d40f6bc12be73491bfc910ab7ae0484cc
mostafa <139650173+Mostafa-Elameer@users.noreply.github.com>
2024-10-16 12:56:24 +0300

    update wishlist becous thiers probelm in json server

--- src/app/allComponents/pages/onemeal/onemeal.component.ts
+++ src/app/allComponents/pages/onemeal/onemeal.component.ts
@@ -3,7 +3,6 @@ import { Component, Input, input, inject } from '@angular/core';
 import { ICategories } from '../../../sheard/interfaces/Icategories';
 import { IoneMeal } from '../../../sheard/interfaces/IOneMeal';
 import { RouterLink } from '@angular/router';
-import { WishlistService } from '../../../sheard/services/wishlist.service';
 import { AllmealsService } from '../../../sheard/services/allmeals.service';
 import { IMealDetils } from '../../../sheard/interfaces/mealDetils';
 import { Iwishlist } from '../../../sheard/interfaces/Iwishlistit';
@@ -24,44 +23,9 @@ export class OnemealComponent {
 
   id: string = ''
 
-  private readonly mywishlist = inject(WishlistService)
   private readonly _AllmealsService = inject(AllmealsService)
 
 
-
-
-  meal: Iwishlist = {} as Iwishlist
-
-  mealId(id: string) {
-
-    let data = {
-      "id": id
-    }
-
-    this.mywishlist.setid(data).subscribe({
-      next: (res) => {
-        this.id = res.id
-        console.log(id);
-
-        this._AllmealsService.gitMealById(id).subscribe({
-          next: (res) => {
-            console.log(res.meals);
-            this.meal = res.meals
-
-            this.mywishlist.setmealwishlist(this.meal).subscribe({
-              next: (res) => {
-                console.log(res);
-              }
-            })
-
-          }
-        })
-
-
-
-      }
-    })
-  }
 
 
 }
--- src/app/allComponents/pages/wishlist/wishlist.component.ts
+++ src/app/allComponents/pages/wishlist/wishlist.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { WishlistService } from '../../../sheard/services/wishlist.service';
 import { AllmealsService } from '../../../sheard/services/allmeals.service';
 
 @Component({
@@ -11,24 +10,6 @@ import { AllmealsService } from '../../../sheard/services/allmeals.service';
 })
 export class WishlistComponent {
 
-
-  allidwishlist: any[] = []
-  private readonly _WishlistService = inject(WishlistService)
-
-
-  allmel: any[] = []
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this._WishlistService.getallmealswishlist().subscribe({
-      next: (res) => {
-        console.log(res);
-        this.allmel = res
-
-      }
-    })
-  }
 
 
 
